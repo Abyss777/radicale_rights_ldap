42e70b746d7593896d816013fa4d0e20b34bdb95
Andrey Kunitsyn <abyss@fox5.ru>
2025-09-12 17:35:44 +0500

    Reverted year

--- radicale_rights_ldap/__init__.py
+++ radicale_rights_ldap/__init__.py
@@ -1,5 +1,5 @@
 # This file is part of Radicale - CalDAV and CardDAV server
-# Copyright © 2024 Andrey Kunitsyn
+# Copyright © 2024-2025 Andrey Kunitsyn
 #
 # This library is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
